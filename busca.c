1b55c400bfeb0caf57a51a3c208cf94e07b54e97
DiogoKraut <diogo.kraut@hotmail.com>
2018-11-20 19:01:01 -0200

    v1.2.1 README added

--- busca.c
+++ busca.c
@@ -15,7 +15,7 @@ int main(int argc, char const *argv[]) {
 	while(1) {
 		fgets(s, MAX_TREEB, stdin); // Le um comando
 		offset = 0;
-		rootB = createBTree(s + 2, &offset);
+		rootB = createBTree(s + 2, &offset); // Pula o comando e o espaco em branco
 
 		switch(*s) {
 			case 'i':
